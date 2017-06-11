5dd1910e905959fbdae2689c18921b7e23972ed5
erhardos <erharddeb@gmail.com>
2017-06-12 00:00:38 +0200

    Refactor users router.

--- routes/posts.js
+++ routes/posts.js
@@ -2,70 +2,13 @@ const express     = require('express'),
   router          = express.Router(),
   passport        = require('passport'),
   passportService = require('../config/passport'),
-  validator       = require('validator'),
-  _               = require('lodash')
-  assert          = require('assert')
-  postService     = require('../services/posts')
-  userService     = require('../services/users')
-  ResponseError   = require('../utils/ResponseError')
+  PostsController = require('../controllers/posts')
 
 const requireAuth = passport.authenticate('jwt', {session: false})
 
-function errorHandler (req, res, err) {
-  if (err.code)
-    return res.status(err.code).json({msg: err.message})
-  res.status(500).json({msg:'Unxepected error!.'})
-}
-
-router.get('/', async (req, res) => {
-  try {
-    const posts = await postService.getAll()
-    res.json({data: posts})
-  } catch (err) {
-    errorHandler(req, res, err)
-  }
-})
-
-router.get('/:id', async (req, res) => {
-  try {
-    const post = await postService.getOneById(req.params.id)
-    if (!post)
-      throw new ResponseError('Post does not exists.', 404)
-    res.json({data: post})
-  } catch (err) {
-    errorHandler(req, res, err)
-  }
-})
-
-router.post('/', requireAuth, async (req, res) => {
-  try {
-    const user = await userService.getUserInfo(req.user)
-    const createdPost = await postService.create({
-      user_id: user.id,
-      title: req.body.title,
-      content: req.body.content
-    })
-    res.json({data: createdPost})
-  } catch (err) {
-    errorHandler(req, res, err)
-  }
-})
-
-router.put('/:id', requireAuth, async (req, res) => {
-  try {
-    const user = await userService.getUserInfo(req.user)
-    const post = await postService.getOneById(req.params.id)
-    if (!post)
-      throw new ResponseError ('Post does not exists.', 404)
-    if (post.get('user_id') !== user.id)
-      throw new ResponseError ('You don\'t have permission to do that.', 401)
-
-    const updatedPost = await post.patch(req.body)
-    res.json({data: updatedPost})
-  } catch (err) {
-    console.log(err)
-    errorHandler(req, res, err)
-  }
-})
+router.get('/', PostsController.getAll)
+router.get('/:id', PostsController.getOne)
+router.post('/', requireAuth, PostsController.create)
+router.put('/:id', requireAuth, PostsController.update)
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,53 +1,19 @@
 const express     = require('express'),
-  fs              = require('fs'),
   multer          = require('multer'),
   router          = express.Router(),
   config          = require('../config/main'),
   jwt             = require('jsonwebtoken'),
   passport        = require('passport'),
   passportService = require('../config/passport'),
-  _               = require('lodash'),
-  uploadAvatar    = require('../services/fileUpload').avatar
-  userService     = require('../services/users')
+  uploadAvatar    = require('../services/fileUpload').avatar,
+  UsersController = require('../controllers/users')
 
 const requireLogin = passport.authenticate('local', { session: false })
 const requireAuth = passport.authenticate('jwt', { session: false })
 
 /* GET users listing. */
-router.get('/all', requireAuth, (req, res) => {
-  userService.getAllUsers().then(function (data) {
-    res.json({data})
-  })
-})
-
-router.get('/avatar', requireAuth,  (req, res) => {
-
-})
-
-router.post('/avatar', requireAuth, uploadAvatar.single('avatar'),  (req, res) => {
-  res.json({msg: 'ok'})
-})
-
-router.post('/adile/', function (req, res) {
-  let text
-
-  try {
-    text = req.body.text
-  } catch (err) {
-    res.json(err)
-  }
-  if (typeof text === 'undefined') {
-    res.json('fail')
-  }
-  fs.writeFile("/tmp/test", text, function (err) {
-    if (err) {
-      res.json(err)
-      return
-    }
-
-    res.json('ok')
-  })
-})
-
+router.get('/all', requireAuth, UsersController.getAll)
+router.get('/:id', requireAuth, UsersController.getOne)
+router.post('/avatar', requireAuth, uploadAvatar.single('avatar'), UsersController.uploadAvatar)
 
 module.exports = router
