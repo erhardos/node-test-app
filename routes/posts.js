0fdf2c5959e7fcc5b8dc3dcd3b1b532f783b18f1
erhardos <erharddeb@gmail.com>
2017-06-11 23:20:55 +0200

    Refactor posts router to use async syntax.

--- routes/posts.js
+++ routes/posts.js
@@ -11,72 +11,61 @@ const express     = require('express'),
 
 const requireAuth = passport.authenticate('jwt', {session: false})
 
-router.get('/', (req, res) => {
-  postService
-    .getAll()
-    .then(data => res.json({data}))
-    .catch(err => {
-      if (err.code)
-        return res.status(err.code).json({msg: err.message})
-      res.status(500).json({msg:'B like broken.'})
-    })
+function errorHandler (req, res, err) {
+  if (err.code)
+    return res.status(err.code).json({msg: err.message})
+  res.status(500).json({msg:'Unxepected error!.'})
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await postService.getAll()
+    res.json({data: posts})
+  } catch (err) {
+    errorHandler(req, res, err)
+  }
 })
 
-router.get('/:id', (req, res) => {
-  postService
-    .getOneById(req.params.id)
-    .then(data => {
-      if (data)
-        res.json({data})
-      else
-        throw new ResponseError('Post does not exists.', 404)
-    })
-    .catch(err => {
-      if (err.code)
-        return res.status(err.code).json({msg: err.message})
-      res.status(500).json({msg:'B like broken.'})
-    })
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await postService.getOneById(req.params.id)
+    if (!post)
+      throw new ResponseError('Post does not exists.', 404)
+    res.json({data: post})
+  } catch (err) {
+    errorHandler(req, res, err)
+  }
 })
 
-router.post('/', requireAuth, (req, res) => {
-  userService
-    .getUserInfo(req.user)
-    .then(user => postService.create({user_id: user.id,
-                                      title: req.body.title,
-                                      content: req.body.content})
-    )
-    .then(data => res.json({data}))
-    .catch(err => {
-      if (err.code)
-        return res.status(err.code).json({msg: err.message})
-      res.status(400).json({message: err.message})
+router.post('/', requireAuth, async (req, res) => {
+  try {
+    const user = await userService.getUserInfo(req.user)
+    const createdPost = await postService.create({
+      user_id: user.id,
+      title: req.body.title,
+      content: req.body.content
     })
+    res.json({data: createdPost})
+  } catch (err) {
+    errorHandler(req, res, err)
+  }
 })
 
-router.put('/:id', requireAuth, (req, res) => {
-  let authUser = null
-  userService
-    .getUserInfo(req.user)
-    .then(user => {
-      authUser = user
-      return postService.getOneById(req.params.id)
-    })
-    .then(post => {
-      if (!post)
-        throw new ResponseError ('Post does not exists.', 404)
-      if (post.get('user_id') !== authUser.id)
-        throw new ResponseError ('You don\'t have permission to do that.', 401)
+router.put('/:id', requireAuth, async (req, res) => {
+  try {
+    const user = await userService.getUserInfo(req.user)
+    const post = await postService.getOneById(req.params.id)
+    if (!post)
+      throw new ResponseError ('Post does not exists.', 404)
+    if (post.get('user_id') !== user.id)
+      throw new ResponseError ('You don\'t have permission to do that.', 401)
 
-      return post.patch(req.body)
-    })
-    .then(data => res.status(200).json({data}))
-    .catch(err => {
-      if (err.code)
-        return res.status(err.code).json({msg: err.message})
-      return res.status(500).json({msg:'B like broken.'})
-    })
-
+    const updatedPost = await post.patch(req.body)
+    res.json({data: updatedPost})
+  } catch (err) {
+    console.log(err)
+    errorHandler(req, res, err)
+  }
 })
-
 
 module.exports = router
